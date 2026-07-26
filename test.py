6b2507c269987e6a34e2e37f40ad89434e77123e
unknown <antineutrino146844@gmail.com>
2026-07-26 01:59:29 +0100

    update

--- test.py
+++ test.py
@@ -1,3 +1,24 @@
-a = [1,2,3]
-b= a.reverse()
-print(b)
+def f(prices):
+    ans = 0
+    cost = float("inf")
+    day_1 = 0
+
+    for i in range(len(prices)):
+        cost = min(cost, prices[i])
+
+        if prices[i] - cost > ans:
+            day_1 = i
+            ans = prices[i] - cost
+
+    return ans, day_1
+
+def solution(prices):
+    day_1_profit, day_1 = f(prices)
+    if day_1 < len(prices):
+        prices = prices[day_1:]
+        return f(prices)[0] + day_1_profit
+    else:
+        return day_1_profit
+    
+
+print(solution([3, 3, 5, 0,0,3,1,4]))
